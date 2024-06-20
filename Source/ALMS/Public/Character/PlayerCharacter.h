a7e9847412fb1decf2a6e00d2cedb8c503cc6084
PauloGN <paulorsantos.gnt@outlook.com>
2024-06-19 20:49:51 -0700

    project creation character class

---
+++ Source/ALMS/Public/Character/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ALSCharacterBase.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALMS_API APlayerCharacter : public AALSCharacterBase
+{
+	GENERATED_BODY()
+	
+};
