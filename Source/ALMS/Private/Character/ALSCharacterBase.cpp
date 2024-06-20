87663b72317256ab15203813dfde08a2f92f4be1
PauloGN <paulorsantos.gnt@outlook.com>
2024-06-20 10:42:07 -0700

    Character import base character class set up

--- Source/ALMS/Private/Character/ALSCharacterBase.cpp
+++ Source/ALMS/Private/Character/ALSCharacterBase.cpp
@@ -3,32 +3,21 @@
 
 #include "Character/ALSCharacterBase.h"
 
-// Sets default values
+
 AALSCharacterBase::AALSCharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), "WeaponHandSocket");
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 }
 
-// Called when the game starts or when spawned
+
 void AALSCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
-
-// Called every frame
-void AALSCharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AALSCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
--- Source/ALMS/Public/Character/ALSCharacterBase.h
+++ Source/ALMS/Public/Character/ALSCharacterBase.h
@@ -6,24 +6,20 @@
 #include "GameFramework/Character.h"
 #include "ALSCharacterBase.generated.h"
 
-UCLASS()
+UCLASS(Abstract)
 class ALMS_API AALSCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+
 	AALSCharacterBase();
 
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	UPROPERTY(EditAnywhere, Category = "Combate")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 };
